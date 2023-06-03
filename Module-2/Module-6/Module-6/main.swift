1bc6776d342a7e1add3bdb7edc1f96c762daca79
Rajponkiya <90110417+Rajponkiya@users.noreply.github.com>
2023-06-02 22:14:57 -0700

    Add files via upload

---
+++ Module-2/Module-6/Module-6/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Module-6
+//
+//  Created by MAC on 22/01/23.
+//  WAP to convert integer value into float value.
+
+import Foundation
+
+print("enter two number")
+let a = Float(readLine()!)!
+let b = Float(readLine()!)!
+
+let c = b/a
+
+print(Float(c))
+
---
+++ Module-2/Module2-4/Module2-4/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Module2-4
+//
+//  Created by MAC on 22/01/23.
+//  WAP to find maximum value from three numbers.
+
+import Foundation
+
+print("Enter thre number : ")
+
+print("Enter first number")
+let a = Int(readLine()!)!
+
+print("Enter second number number")
+let b = Int(readLine()!)!
+
+print("Enter Third number")
+let c = Int(readLine()!)!
+
+var maxnumb = max(a,b,c)
+print("your maximum number is = \(maxnumb)")
+
+
