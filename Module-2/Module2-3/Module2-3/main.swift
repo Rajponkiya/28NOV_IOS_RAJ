ecb2b814c385181ecdacd9601659dc44720f6772
Rajponkiya <90110417+Rajponkiya@users.noreply.github.com>
2023-06-02 22:14:19 -0700

    Add files via upload

---
+++ Module-2/Module2-3/Module2-3/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Module2-3
+//
+//  Created by MAC on 22/01/23.
+//WAP to set default value to the variable.
+
+import Foundation
+
+var a=0 ,b=0
+print("enter two number ")
+a = Int(readLine()!)!
+b = Int(readLine()!)!
+
+let c = Int(a+b)
+
+print("your sum of two number is = \(c)")
