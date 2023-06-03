4cc6c52d09e8f40bf51795d6d7e612c5c6b64b30
Rajponkiya <90110417+Rajponkiya@users.noreply.github.com>
2023-06-02 22:13:50 -0700

    Add files via upload

---
+++ Module-2/Module2-2/Module2-2/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Module2-2
+//
+//  Created by MAC on 22/01/23.
+// WAP to take an integer value from user.
+
+import Foundation
+
+print("enter you contect number")
+if
+let Number = Int(readLine()!)
+{
+print("your number is \(Number)")
+}
+else
+{
+    print("enter integer number ")
+}
