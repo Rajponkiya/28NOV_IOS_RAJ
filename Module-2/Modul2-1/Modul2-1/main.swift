a09e1eca670c436479c10d5f75bb7094fc9cf07a
Rajponkiya <90110417+Rajponkiya@users.noreply.github.com>
2023-06-02 22:13:18 -0700

    Add files via upload

---
+++ Module-2/Modul2-1/Modul2-1/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Modul2-1
+//
+//  Created by MAC on 22/01/23.
+//WAP to take a string value from user.
+
+import Foundation
+
+print("enter your name :")
+if
+let name = readLine()
+{
+print("your name is\(name)")
+}
