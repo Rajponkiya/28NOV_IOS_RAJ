67b2009ce5c9312438e8ef74afdfb6ae255562a9
Rajponkiya <90110417+Rajponkiya@users.noreply.github.com>
2023-06-02 22:18:03 -0700

    Add files via upload

---
+++ Module-2/Module2-14/Module2-14/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Module2-14
+//
+//  Created by MAC on 23/01/23.
+// WAP to perform operation like. Union, intersection etc. using
+// set.
+
+import Foundation
+
+
+var setname: Set<String> = [ "a", "b", "c", "d", "e"]
+
+print(setname)
